817c5d8672a77d80b0e12fbb2f7092f4cf670492
Merkwurdichliebe <11777208+Merkwurdichliebe@users.noreply.github.com>
2021-09-05 22:31:16 +0200

    Add progress bar

--- js/irregular-verbs.js
+++ js/irregular-verbs.js
@@ -18,14 +18,15 @@ function getData(url) {
 async function main() {
     // Get verbs list
     verbs = await getData('js/verbs-list.json')
+    console.log('Loaded ' + verbs.length + ' verbs.')
 
     // Globally assign the Enter key to the button click event
     window.addEventListener('keyup', function(event) {
         if (event.key === 'Enter') {
             // Cancel the default action, if needed
-            event.preventDefault();
+            event.preventDefault()
             // Trigger the button element with a click
-            document.getElementById('button').click();
+            document.getElementById('button').click()
         }
     })
 
@@ -39,6 +40,7 @@ function newGame() {
     document.getElementById('message').innerHTML = ''
     score = 0
     questionCount = 0
+    initProgressBar()
     nextVerb()
 }
 
@@ -55,6 +57,21 @@ function nextVerb() {
     document.getElementById('button').removeEventListener('click', nextVerb)
     document.getElementById('button').value = 'Check'
     document.getElementById('button').addEventListener('click', check)
+}
+
+function initProgressBar() {
+    let div = document.getElementById('progress')
+    div.innerHTML = ''
+    for (let i=0; i < maxQuestions; i++) {
+        let el = document.createElement('div')
+        let id = 'progress-item-' + String(i)
+        el.setAttribute('id', id)
+        el.setAttribute('class', 'progress-item')
+        el.setAttribute('style', 'grid-column: ' + String(i+1))
+        el.classList.add('hidden')
+        div.appendChild(el)
+        el.innerHTML = ''
+    }
 }
 
 function getUserInputFields() {
@@ -81,14 +98,20 @@ function check() {
     // separated by a slash
     let correctAnswer = [currentVerb[1].split('/'), currentVerb[2].split('/')]
 
+    // Unhide progress bar item
+    let grid_id = 'progress-item-' + String(questionCount - 1)
+    document.getElementById(grid_id).classList.remove('hidden')
+
     // Check both answers
     for (let i = 0; i < 2; i++) {
         user_input[i].classList.remove('default')
         if (correctAnswer[i].includes(user_input[i].value.toLowerCase())) {
             score += 1
             user_input[i].classList.add('correct')
+            document.getElementById(grid_id).classList.add('correct')
         } else {
             user_input[i].classList.add('incorrect')
+            document.getElementById(grid_id).classList.add('incorrect')
         }
         user_input[i].value = correctAnswer[i]
     }
@@ -110,7 +133,7 @@ function check() {
 }
 
 function updateScore() {
-    document.getElementById('score').innerHTML = 'Score : ' + score
+    document.getElementById('score').innerHTML = score
 }
 
 function pause() {
@@ -126,7 +149,7 @@ function gameOver() {
         msg += 'Well done! '
     }
     msg += 'Press Start to play again.'
-    document.getElementById('score').innerHTML = msg
+    document.getElementById('message').innerHTML = msg
     let button = document.getElementById('button')
     button.removeEventListener('click', nextVerb)
     button.value = 'Start'
